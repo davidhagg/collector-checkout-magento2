f8c58c0682894ad2daf1a0327bf2eb5be370d70b
Kristoffer Karlsson <kristoffer@webbhuset.se>
2019-08-09 08:47:43 +0200

    Added method switcher to command pool in execute method

--- src/Gateway/Command/CollectorBankCommand.php
+++ src/Gateway/Command/CollectorBankCommand.php
@@ -6,9 +6,20 @@ use Magento\Payment\Gateway\CommandInterface as CommandInterface;
 
 class CollectorBankCommand implements CommandInterface
 {
+
+    protected $method;
+
+    public function __construct($client)
+    {
+        $this->method = $client['method'];
+    }
+
     public function execute(array $commandSubject)
     {
-        return false;
+        $method = $this->method;
+        if (method_exists($this, $method)) {
+            call_user_func([$this, $method], $commandSubject);
+        }
     }
 
     public function capture($data)
