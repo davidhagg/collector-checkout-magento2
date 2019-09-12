4050bec3ff045d3eb72b2c5aeb6359444066d312
Jens Pettersson <jens@webbhuset.se>
2019-09-12 13:20:56 +0000

    on quote save, update collector order

---
+++ src/Observer/QuoteSaveObserver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Webbhuset\CollectorBankCheckout\Observer;
+
+class QuoteSaveObserver implements \Magento\Framework\Event\ObserverInterface
+{
+    protected $config;
+    protected $adpter;
+
+    public function __construct(
+        \Webbhuset\CollectorBankCheckout\Config\Config $config,
+        \Webbhuset\CollectorBankCheckout\Adapter $adapter
+    ) {
+        $this->config = $config;
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * On quote save, update collector order
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $quote = $observer->getQuote();
+        if (!$quote->getIsActive() || !$this->config->getIsActive()) {
+            return;
+        }
+
+        $this->adapter->updateCart($quote);
+        $this->adapter->updateFees($quote);
+    }
+}
